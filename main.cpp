4d5ffc2fc88b836fd14b7a9cf684b24722d4d5d4
Ghatan55 <ghatanhardanni@gmail.com>
2025-05-08 10:13:28 +0700

    Menambahkan fungsi void addnode

--- main.cpp
+++ main.cpp
@@ -18,4 +18,27 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikat noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next =START;
+            START = nodeBaru;
+            return;
+        }
+        
+    }
 };
